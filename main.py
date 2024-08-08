947b2461249dc865255370d4138de38b4e281de8
David Reverter Valeiras <d.r.valeiras@gmail.com>
2024-08-08 23:07:54 +0200

    Adds some new endpoints

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from pydantic import BaseModel
+from typing import Annotated
 
-app = FastAPI()
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int | None = None
+    
+app = FastAPI(title="Greeter app")
 
 @app.get("/")
 async def root():
-    return {"message": "Hello Worldly creatures"}
+    return {"message": "Hello, this is a fastAPI app"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/greet/{name}")
+async def greet(name: str):
+    return {"message": "Hello, {}, this is a fastAPI app".format(name)}
 
-@app.get("/lookup")
-async def lookup_item(q, limit: int= 5):
-    return {"q": q, "limit": limit}
+@app.get("/greet-many-times")
+async def greet_many_times(n: int = 4):
+    message = ""
+    for i in range(n):
+        message += "Hello, this is a fastAPI app. "
+    return {"message": message}
+
+@app.post("/greet-a-person/")
+async def greet_a_person(person: Person):
+    message = "Hello, {} {}, ".format(person.first_name, person.last_name)
+    if(person.age):
+        message += "I know you are {} years old".format(person.age)
+    else:
+        message += "I do not know how old you are"
+        
+    return {"message": message}
+
+@app.post("/greet-a-form/")
+async def greet_a_form(first_name: Annotated[str, Form()], last_name: Annotated[str, Form()]):
+    return {"message": "Hello, {} {}, you really nailed that form".format(first_name, last_name)}
